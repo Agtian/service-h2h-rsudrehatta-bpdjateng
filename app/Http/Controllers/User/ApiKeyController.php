b098cbe2eb923420733519fc9b56eb68b85bb7db
agtianricky <agtianrickey33@gmail.com>
2024-03-13 09:58:11 +0700

    created menu api key user

--- app/Http/Controllers/User/ApiKeyController.php
+++ app/Http/Controllers/User/ApiKeyController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\ApiKey;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ApiKeyController extends Controller
 {
     public function index()
     {
-        return view("template-dashboard.layouts.api-key.index");
+        return view("template-dashboard.layouts.api-key.index", [
+            'detailApiKey' => ApiKey::where('user_id', Auth::user()->id)->first(),
+        ]);
     }
 }
--- resources/views/template-dashboard/layouts/api-key/index.blade.php
+++ resources/views/template-dashboard/layouts/api-key/index.blade.php
@@ -9,26 +9,64 @@
                     <div class="col-6 d-flex align-items-center">
                         <h6 class="mb-0">API KEY Method</h6>
                     </div>
-                    <div class="col-6 text-end">
-                        <a class="btn bg-gradient-dark mb-0" href="javascript:;"><i class="fas fa-plus"></i>&nbsp;&nbsp;Add New Card</a>
-                    </div>
                 </div>
             </div>
             <div class="card-body p-3">
                 <div class="row">
-                    <div class="col-md-6 mb-md-0 mb-4">
+                    <div class="col-md-12">
                         <div class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row">
-                            <img class="w-10 me-3 mb-0" src="{{ asset('asset/img/logos/mastercard.png') }}" alt="logo">
-                            <h6 class="mb-0">****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;7852</h6>
-                            <i class="fas fa-pencil-alt ms-auto text-dark cursor-pointer" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Card"></i>
+                            <img class="w-3 me-3 mb-0" src="{{ asset('asset/img/logos/key.png')}}" alt="logo">
+                            <h6 class="mb-0">{{ $detailApiKey->key }}</h6>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row">
-                            <img class="w-10 me-3 mb-0" src="{{ asset('asset/img/logos/visa.png')}}" alt="logo">
-                            <h6 class="mb-0">****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;5248</h6>
-                            <i class="fas fa-pencil-alt ms-auto text-dark cursor-pointer" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Card"></i>
-                        </div>
+                </div>
+            </div>
+            <div class="card-body p-3">
+                <div class="col-6 d-flex align-items-center">
+                    <h6 class="mb-2">User Information</h6>
+                </div>
+                <div class="row g-3 mb-2 align-items-center">
+                    <div class="col-md-3">
+                        <label for="name" class="col-form-label">User</label>
+                    </div>
+                    <div class="col-md-9">
+                        <input type="text" id="name" class="form-control" readonly value="{{ optional($detailApiKey->getUser)->name }}">
+                    </div>
+                </div>
+                <div class="row g-3 mb-2 align-items-center">
+                    <div class="col-md-3">
+                        <label for="company_name" class="col-form-label">Company</label>
+                    </div>
+                    <div class="col-md-9">
+                        <input type="text" id="company_name" class="form-control" readonly value="{{ $detailApiKey->company_name }}">
+                    </div>
+                </div>
+                <div class="row g-3 mb-2 align-items-center">
+                    <div class="col-md-3">
+                        <label for="project_name" class="col-form-label">Project Name</label>
+                    </div>
+                    <div class="col-md-9">
+                        <input type="text" id="project_name" class="form-control" readonly value="{{ $detailApiKey->project_name }}">
+                    </div>
+                </div>
+                <div class="row g-3 mb-2 align-items-center">
+                    <div class="col-md-3">
+                        <label for="key" class="col-form-label">API Key</label>
+                    </div>
+                    <div class="col-md-9">
+                        <input type="text" id="key" class="form-control" readonly value="{{ $detailApiKey->key }}">
+                    </div>
+                </div>
+                <div class="row g-3 mb-2 align-items-center">
+                    <div class="col-md-3">
+                        <label for="status" class="col-form-label">Status API Key</label>
+                    </div>
+                    <div class="col-md-9">
+                        @if ($detailApiKey->status_api_key == 1)
+                            <button id="status" class="btn btn-sm btn-success">Aktif</button>
+                        @else
+                            <button id="status" class="btn btn-sm btn-danger">Tidak Aktif</button>
+                        @endif
                     </div>
                 </div>
             </div>
