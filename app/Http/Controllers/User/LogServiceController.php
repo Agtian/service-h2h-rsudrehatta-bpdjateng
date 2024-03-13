d6e86db503c15bfb1fdd38760579172fcbcbc93d
agtianricky <agtianrickey33@gmail.com>
2024-03-13 10:11:03 +0700

    Modified table log service user

--- app/Http/Controllers/User/LogServiceController.php
+++ app/Http/Controllers/User/LogServiceController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\ApiEventHistories;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogServiceController extends Controller
 {
     public function index()
     {
-        return view("template-dashboard.layouts.log-service.index");
+        return view("template-dashboard.layouts.log-service.index", [
+            'resultLogServices' => ApiEventHistories::where('api_key_id', Auth::user()->id)->paginate()
+        ]);
     }
 }
