ec4df650359f21c86a32eea82ebdab68852de1a7
agtianricky <agtianrickey33@gmail.com>
2024-03-20 11:08:08 +0700

    Modifeid dashboad user

--- app/Http/Controllers/Dashboard/UserDashboardController.php
+++ app/Http/Controllers/Dashboard/UserDashboardController.php
@@ -3,18 +3,35 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\ApiEventHistories;
+use App\Models\ApiKey;
 use App\Models\LogPayment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class UserDashboardController extends Controller
 {
     public function index()
     {
+        $apiKey = ApiKey::where([
+            ['project_id', 1],
+            ['user_id', Auth::user()->id]
+        ])->first();
+
+        $getHistoryApiAll = DB::select("SELECT MONTHNAME(created_at) as bulan, COUNT(id) AS used_quantity
+                    FROM api_event_histories
+                    WHERE YEAR(created_at) = YEAR(CURDATE())
+                        AND api_key_id = $apiKey->id
+                    GROUP BY MONTHNAME(created_at)");
+
         return view('template-dashboard.layouts.dashboard.user', [
-            'countBillData'                 => LogPayment::whereYear('created_at', date('Y'))->count(),
-            'countPaymentCompleted'         => LogPayment::whereYear('created_at', date('Y'))->where('status_payment', 1)->count(),
-            'countResponseConnected'        => LogPayment::whereYear('created_at', date('Y'))->where('status_reversal', 1)->count(),
-            'countResponseConnectionFailed' => LogPayment::whereYear('created_at', date('Y'))->where('status_reversal', 0)->count(),
+            'countBillData'                 => LogPayment::where('api_key_id', $apiKey->id)->whereYear('created_at', date('Y'))->count(),
+            'countPaymentCompleted'         => LogPayment::where('api_key_id', $apiKey->id)->whereYear('created_at', date('Y'))->where('status_payment', 1)->count(),
+            'countResponseConnected'        => LogPayment::where('api_key_id', $apiKey->id)->whereYear('created_at', date('Y'))->where('status_reversal', 1)->count(),
+            'countResponseConnectionFailed' => LogPayment::where('api_key_id', $apiKey->id)->whereYear('created_at', date('Y'))->where('status_reversal', 0)->count(),
+            'getHistoryApiAll'              => $getHistoryApiAll,
+            'getCountHistoryApiAll'         => ApiEventHistories::where('api_key_id', $apiKey->id)->whereYear('created_at', date('Y'))->count(),
         ]);
     }
 }
--- resources/views/template-dashboard/layouts/dashboard/user.blade.php
+++ resources/views/template-dashboard/layouts/dashboard/user.blade.php
@@ -10,7 +10,7 @@
                             <div class="numbers">
                                 <p class="text-sm mb-0 text-uppercase font-weight-bold">Get bill data</p>
                                 <h5 class="font-weight-bolder">
-                                    $53,000
+                                    {{ $countBillData }}
                                 </h5>
                                 <p class="mb-0">
                                     <span class="text-success text-sm font-weight-bolder"></span>
@@ -35,7 +35,7 @@
                             <div class="numbers">
                                 <p class="text-sm mb-0 text-uppercase font-weight-bold">Payment completed</p>
                                 <h5 class="font-weight-bolder">
-                                    2,300
+                                    {{ $countPaymentCompleted }}
                                 </h5>
                                 <p class="mb-0">
                                     <span class="text-success text-sm font-weight-bolder">+3%</span>
@@ -60,7 +60,7 @@
                             <div class="numbers">
                                 <p class="text-sm mb-0 text-uppercase font-weight-bold">RESPONSE CONNECTED</p>
                                 <h5 class="font-weight-bolder">
-                                    +3,462
+                                    {{ $countResponseConnected }}
                                 </h5>
                                 <p class="mb-0">
                                     <span class="text-danger text-sm font-weight-bolder"></span>
@@ -85,7 +85,7 @@
                             <div class="numbers">
                                 <p class="text-sm mb-0 text-uppercase font-weight-bold">RESPONSE CONNECTION FAILED</p>
                                 <h5 class="font-weight-bolder">
-                                    $103,430
+                                    {{ $countResponseConnectionFailed }}
                                 </h5>
                                 <p class="mb-0">
                                     <span class="text-success text-sm font-weight-bolder"></span>this year {{ date('Y') }}
@@ -106,10 +106,10 @@
         <div class="col-lg-12 mb-lg-0 mb-4">
             <div class="card z-index-2 h-100">
                 <div class="card-header pb-0 pt-3 bg-transparent">
-                    <h6 class="text-capitalize">Sales overview</h6>
+                    <h6 class="text-capitalize">Count Request API</h6>
                     <p class="text-sm mb-0">
                         <i class="fa fa-arrow-up text-success"></i>
-                        <span class="font-weight-bold">4% more</span> in 2021
+                        <span class="font-weight-bold">{{ $getCountHistoryApiAll }} request</span> in {{ date('Y') }}
                     </p>
                 </div>
                 <div class="card-body p-3">
@@ -121,3 +121,95 @@
         </div>
     </div>
 @endsection
+
+@push('script')
+    <script>
+        var ctx1 = document.getElementById("chart-line").getContext("2d");
+
+        var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
+
+        gradientStroke1.addColorStop(1, 'rgba(94, 114, 228, 0.2)');
+        gradientStroke1.addColorStop(0.2, 'rgba(94, 114, 228, 0.0)');
+        gradientStroke1.addColorStop(0, 'rgba(94, 114, 228, 0)');
+        new Chart(ctx1, {
+            type: "line",
+            data: {
+                labels: [@foreach ($getHistoryApiAll as $item)
+                            '@php echo "$item->bulan"; @endphp',
+                        @endforeach],
+                        // ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                datasets: [{
+                    label: "Quantity Request",
+                    tension: 0.4,
+                    borderWidth: 0,
+                    pointRadius: 0,
+                    borderColor: "#5e72e4",
+                    backgroundColor: gradientStroke1,
+                    borderWidth: 3,
+                    fill: true,
+                    data: [@foreach ($getHistoryApiAll as $item)
+                            '@php echo "$item->used_quantity"; @endphp',
+                        @endforeach],
+                    // [50, 40, 300, 220, 500, 250, 400, 230, 500],
+                    maxBarThickness: 6
+
+                }],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: false,
+                    }
+                },
+                interaction: {
+                    intersect: false,
+                    mode: 'index',
+                },
+                scales: {
+                    y: {
+                        grid: {
+                            drawBorder: false,
+                            display: true,
+                            drawOnChartArea: true,
+                            drawTicks: false,
+                            borderDash: [5, 5]
+                        },
+                        ticks: {
+                            display: true,
+                            padding: 10,
+                            color: '#fbfbfb',
+                            font: {
+                            size: 11,
+                            family: "Open Sans",
+                            style: 'normal',
+                            lineHeight: 2
+                            },
+                        }
+                },
+                x: {
+                    grid: {
+                        drawBorder: false,
+                        display: false,
+                        drawOnChartArea: false,
+                        drawTicks: false,
+                        borderDash: [5, 5]
+                    },
+                    ticks: {
+                        display: true,
+                        color: '#ccc',
+                        padding: 20,
+                        font: {
+                            size: 11,
+                            family: "Open Sans",
+                            style: 'normal',
+                            lineHeight: 2
+                        },
+                    }
+                },
+                },
+            },
+        });
+    </script>
+@endpush
